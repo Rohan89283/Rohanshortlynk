5641d9898615587286158f212ba6f4b13771743b
Rohan89283 <rohanafk@black.com>
2026-04-22 00:47:34 +0530

    Updated main.py

--- main.py
+++ main.py
@@ -32,13 +32,7 @@ import nest_asyncio
 import names
 import psutil
 
-from selenium import webdriver
-from selenium.webdriver.chrome.service import Service
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.common.action_chains import ActionChains
-from selenium.webdriver.common.keys import Keys
+from playwright.sync_api import sync_playwright, Page, Browser, BrowserContext, TimeoutError as PWTimeoutError
 from fake_useragent import UserAgent
 
 # Keep library logs quiet (Railway/container)
